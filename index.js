4e47eb845e6e734d8a23032b2f8075cbef89dc62
sajjathossainbd <sajjadsadiq2020@gmail.com>
2024-08-19 18:56:46 +0600

    mongodb connected and product data database to load server

--- index.js
+++ index.js
@@ -9,6 +9,35 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8i4eibr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    const productCollection = client.db("sadhinmart").collection("products");
+
+    // PRODUCT API
+    app.get("/products", async (req, res) => {
+      const result = await productCollection.find().toArray();
+      res.send(result);
+    });
+
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Welcome to Sadhin Mart Server Is Running");
 });
